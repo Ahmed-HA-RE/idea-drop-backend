d12d2fd425f2caeedd2db7e7014b6aaeb60ccdad
Ahmed-HA-RE <ah607k@gmail.com>
2025-08-21 16:56:46 +0400

    feat:add error handling middlware function

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import ideaRoutes from './routes/ideaRoutes.js';
+import { errorHandler } from './middleware/errorHandler.js';
 
 dotenv.config();
 const PORT = process.env.PORT || 8000;
@@ -14,6 +15,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/ideas', ideaRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalURL}`);
+  error.statusCode = 400;
+
+  next(error);
+});
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
