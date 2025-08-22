5c1aa5ab95ca7cca0bd365fffb42fc04776b644c
Ahmed-HA-RE <ah607k@gmail.com>
2025-08-22 12:23:23 +0400

    feat:add PUT method to update idea

--- routes/ideaRoutes.js
+++ routes/ideaRoutes.js
@@ -74,4 +74,70 @@ router.post('/', async (req, res, next) => {
   }
 });
 
+//@route        DELETE /api/ideas/:id
+//@description  Delete idea
+//@access       Public
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      const err = new Error('Idea was not found 🙂');
+      err.statusCode = 404;
+      throw err;
+    }
+
+    const idea = await Idea.findByIdAndDelete(id);
+
+    if (!idea) {
+      const err = new Error('Idea was not found 🙂');
+      err.statusCode = 404;
+      throw err;
+    }
+
+    res.status(200).json({ message: 'Idea was deleted succssfully ✅' });
+  } catch (error) {
+    next(error);
+  }
+});
+
+//@route        PUT /api/ideas/:id
+//@description  Edit idea
+//@access       Public
+router.put('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { title, description, summary, tags } = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      const err = new Error('Idea was not found 🙂');
+      err.statusCode = 404;
+      throw err;
+    }
+
+    if (!title?.trim() || !summary?.trim() || !description?.trim()) {
+      const error = new Error('Please fill in the fields');
+      error.statusCode = 400;
+      throw error;
+    }
+
+    const updateIdea = await Idea.findByIdAndUpdate(
+      id,
+      {
+        title,
+        summary,
+        description,
+        tags: Array.isArray(tags)
+          ? tags
+          : tags.split(',').map((tag) => tag.trim()),
+      },
+      { new: true, runValidators: true }
+    );
+
+    res.status(200).json(updateIdea);
+  } catch (error) {
+    next(error);
+  }
+});
+
 export default router;
