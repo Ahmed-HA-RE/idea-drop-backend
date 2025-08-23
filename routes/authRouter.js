c8d96e30db6f61cbef4e24a1b7eadead50896c1b
Ahmed-HA-RE <ah607k@gmail.com>
2025-08-23 14:49:07 +0400

    feat:create route for login

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -49,11 +49,62 @@ router.post('/register', async (req, res, next) => {
   }
 });
 
+//@route            POST /api/auth/login
+//@description      Authenticate user
+//@access           Public
+
+router.post('/login', async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      const err = new Error('Email and password are required');
+      err.statusCode = 400;
+      throw err;
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      const err = new Error('Invalid Credentials');
+      err.statusCode = 400;
+      throw err;
+    }
+
+    const isMatch = await user.matchPassword(password);
+
+    if (!isMatch) {
+      const err = new Error('Invalid Credentials');
+      err.statusCode = 400;
+      throw err;
+    }
+
+    const playload = { userId: user._id.toString() };
+    const accessToken = await generateToken(playload, '1m');
+    const refreshToken = await generateToken(playload, '30d');
+
+    res.cookie('refreshToken', refreshToken, {
+      httpOnly: true,
+      maxAge: 30 * 24 * 60 * 1000,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'none',
+    });
+
+    res.status(201).json({
+      accessToken,
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
 //@route            POST /api/auth/logout
 //@description      logout user and clear token
 //@access           Private
-
-router.post('/logout', (req, res, next) => {
+router.post('/logout', async (req, res, next) => {
   res.clearCookie('refreshToken', {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
